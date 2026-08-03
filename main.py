a98b78527a525924f86a7afae217cf1fa8b09a83
Atul <134540670+AtulPahal@users.noreply.github.com>
2026-08-03 09:13:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 16 # how many independent sequences will we process in parallel?
+batch_size = 16 # how many independent sequences will we process in parallel? 
 block_size = 32 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 100
